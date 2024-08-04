5d052ecfdabdeb9dc104b409a262fd4a65279521
ARGOSTA <arg7971@gmail.com>
2024-08-04 18:19:41 -0500

    removed unnecessary test logs

--- testbed/src/game.c
+++ testbed/src/game.c
@@ -4,14 +4,6 @@
 
 b8 game_initialize(game *game_inst) {
     KDEBUG("game_initialize() called.");
-
-    KFATAL("A test message: %f", 3.14f);
-    KERROR("A test message: %f", 3.14f);
-    KWARN("A test message: %f", 3.14f);
-    KINFO("A test message: %f", 3.14f);
-    KDEBUG("A test message: %f", 3.14f);
-    KTRACE("A test message: %f", 3.14f);
-
     return TRUE;
 }
 
